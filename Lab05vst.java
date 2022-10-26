81932b7bea5e9760e80fc5837abef5efd7b7dc6b
Aydin <70951773+AydinT222@users.noreply.github.com>
2022-10-26 13:12:19 -0400

    i did

--- Lab05vst.java
+++ Lab05vst.java
@@ -17,5 +17,21 @@ public class Lab05vst extends Applet
         int x2 = 990;
         int y2 = 640;
         g.drawRect(10,10,width,height);
+        g.setColor(Color.BLUE);
+        for (int k = 0; k < 50; k++) {
+            //g.drawLine(x1 + k, y1 ,x2, clamp(y2 - k, 650, 100));
+            g.drawLine(
+                    x1,
+                    y1,
+                    x2,
+                    y2
+            );
+            x1 += 20;
+            y2 -= 10;
+            //368
+        }
+        /*for (int k = 1000; k > 0; k-= 10) {
+            g.drawLine(x1 + k, y1, x2, y2 - k);
+        }*/
     }
 }
